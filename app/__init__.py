037241b9f5898abb806676c27401ccfeb88f8678
davidmaina085@gmail.com <davidmaina085@gmail.com>
2020-09-15 11:17:12 +0300

    deployment to heroku

--- app/__init__.py
+++ app/__init__.py
@@ -12,7 +12,7 @@ def create_app(config_name):
     # Creating the app configurations
 
     app.config.from_object(config_options[config_name])
-    config_options[config_name].init_app(app) requirements.txt
+    # config_options[config_name].init_app(app) requirements.txt
 
     # Initializing flask extensions
     bootstrap.init_app(app)
@@ -25,8 +25,6 @@ def create_app(config_name):
     # setting config
     from .requests import configure_request
     configure_request(app)
-
-
 
 
     return app
