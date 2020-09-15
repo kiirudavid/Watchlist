ae03e9e9ac560d98501581edf79c87a77e8c9643
davidmaina085@gmail.com <davidmaina085@gmail.com>
2020-09-15 08:50:31 +0300

    heroku setup

---
+++ config.py
@@ -0,0 +1,26 @@
+# MOVIE_API_KEY = '<c85da0de991a50053336d90226dd3651>'
+# MOVIE_API_KEY = '<Movie API Key>'
+# SECRET_KEY = '<Flask WTF Secret Key>'
+
+
+
+import os
+
+class Config:
+
+    MOVIE_API_BASE_URL ='https://api.themoviedb.org/3/movie/{}?api_key={}'
+    MOVIE_API_KEY = os.environ.get('c85da0de991a50053336d90226dd3651')
+    SECRET_KEY = os.environ.get('Flask WTF Secret Key')
+
+
+class ProdConfig(Config):
+    pass
+
+
+class DevConfig(Config):
+    DEBUG = True
+
+config_options = {
+'development':DevConfig,
+'production':ProdConfig
+}
