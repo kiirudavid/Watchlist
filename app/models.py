ee5bb691f59ca0c850c9567d37f18172200cd47d
davidmaina085@gmail.com <davidmaina085@gmail.com>
2020-09-17 09:01:03 +0300

    added user authentication

--- app/models.py
+++ app/models.py
@@ -1,3 +1,12 @@
+from . import db
+from werkzeug.security import generate_password_hash,check_password_hash
+from flask_login import UserMixin
+from . import login_manager
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
 class Movie:
     '''
     Movie class to define Movie Objects
@@ -10,34 +19,51 @@ class Movie:
         self.vote_average = vote_average
         self.vote_count = vote_count
 
-
-
 class Review:
-
     all_reviews = []
-
-def __init__(self,movie_id,title,imageurl,review):
+    def __init__(self,movie_id,title,imageurl,review):
         self.movie_id = movie_id
         self.title = title
         self.imageurl = imageurl
         self.review = review
-
-
-def save_review(self):
+    def save_review(self):
         Review.all_reviews.append(self)
-
-
-@classmethod
-def clear_reviews(cls):
+    @classmethod
+    def clear_reviews(cls):
         Review.all_reviews.clear()
-
-@classmethod
-def get_reviews(cls,id):
-
+    @classmethod
+    def get_reviews(cls,id):
         response = []
-
         for review in cls.all_reviews:
             if review.movie_id == id:
                 response.append(review)
-
         return response
+
+class User(UserMixin,db.Model):
+        __tablename__ = 'users'
+    
+    id = db.Column(db.Integer,primary_key = True)
+    username = db.Column(db.String(255),index = True)
+    email = db.Column(db.String(255),unique = True,index = True)
+    role_id = db.Column(db.Integer,db.ForeignKey('roles.id'))
+    password_hash = db.Column(db.String(255))
+
+    @property
+    def password(self):
+        raise AttributeError('You cannot read the password attribute')
+    @password.setter
+    def password(self, password):
+        self.pass_secure = generate_password_hash(password)
+    def verify_password(self,password):
+        return check_password_hash(self.pass_secure,password)
+    def __repr__(self):
+        return f'User {self.username}'
+
+class Role(db.Model):
+    __tablename__ = 'roles'
+
+    id = db.Column(db.Integer,primary_key = True)
+    name = db.Column(db.String(255))
+    users = db.relationship('User',backref = 'role',lazy="dynamic")
+    def __repr__(self):
+        return f'User {self.name}'
--- config.py
+++ config.py
@@ -10,7 +10,8 @@ class Config:
     MOVIE_API_BASE_URL ='https://api.themoviedb.org/3/movie/{}?api_key={}'
     MOVIE_API_KEY = os.environ.get('c85da0de991a50053336d90226dd3651')
     SECRET_KEY = os.environ.get('Flask WTF Secret Key')
-
+    # SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://username:password@localhost/watchlist'
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:maich000@localhost/watchlist'
 
 class ProdConfig(Config):
     pass
